a5f2f885a24cbf283656da63c734295f085fb998
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2024-05-10 18:30:47 +0900

    Feat | 각종 스크립트 추가 및 이상현상 판정, 추첨 방식 구현

---
+++ Assets/Scripts/PhenomenonManagement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhenomenonManagement : MonoBehaviour
+{
+    public string PhenomenonName = "";
+    public bool IsNormal = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

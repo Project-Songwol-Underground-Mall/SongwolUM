1e9321ced57c29a4d450fa207c5967d7114eaf70
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2024-05-12 21:35:25 +0900

    Refactor | CharacterMovement 코드 리팩토링

--- Assets/Scripts/GamePlayManager.cs
+++ Assets/Scripts/GamePlayManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GamePlayManager : MonoBehaviour
 {
+    public GameObject SpawnManager;
     public bool CanTeleport = true;
 
     int CurrentStage = 0; // 현재 구역 번호
@@ -75,7 +76,6 @@ public class GamePlayManager : MonoBehaviour
             IsNormalStage = true;
         }
 
-
         else if (Result <= Boundary || IsNormalStage) // 이전 스테이지가 일반 스테이지거나 추첨 결과가 이상현상 스테이지
         {
             Debug.Log("이번 스테이지는 이상현상 스테이지입니다.");
@@ -86,9 +86,11 @@ public class GamePlayManager : MonoBehaviour
             }
             IsAbnormalOccured[AbnormalNumber] = true;
             IsNormalStage = false;
-            // 여기서 추첨 결과로 나온 번호의 오브젝트의 이상현상 발생 Version을 Spawn해줘야 한다.
-
         }
+        // 여기서 추첨 결과로 나온 번호의 오브젝트의 이상현상 발생 Version을 Spawn해줘야 한다.
+        SpawnManager.GetComponent<PhenomenonManagement>().SpawnPhenomenon(AbnormalNumber, IsNormalStage);
+
+
 
         // 이상현상 Version의 오브젝트를 제외한 나머지 오브젝트를 스폰해준다. 스폰해두고 남겨놓는 방법도 고려중.
         // 대신 그러면 이상현상 발생 오브젝트의 이전 버전은 지워주고, 이전에 발생한 이상현상 오브젝트도 지워주고 정상버전으로 다시 Spawn하는 수고가 필요하다.

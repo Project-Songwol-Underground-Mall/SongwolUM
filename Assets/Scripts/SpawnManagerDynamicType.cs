b18455b0f48a88d86629ea205679f0fc6bc2d8d7
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2024-06-02 17:33:03 +0900

    Feat | PhenomenonManagement 구조 변경

--- Assets/Scripts/SpawnManagerDynamicType.cs
+++ Assets/Scripts/SpawnManagerDynamicType.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class SpawnManagerDynamicType : MonoBehaviour
 {
-    public bool IsNormal = true;
+    public GameObject Mannequin;
+    public GameObject SuitMan;
+    public GameObject Zombie;
+    public GameObject LightSystem;
+    public GameObject Floor;
+    public AudioClip SirenSound;
+    public AudioClip GhostSound;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +21,57 @@ public class SpawnManagerDynamicType : MonoBehaviour
     void Update()
     {
         
+    }
+    
+    public void ActivateAP(int PhenomenonNumber, bool isNormal)
+    {
+        if (PhenomenonNumber == 6) AnimateMannequin();
+        if (PhenomenonNumber == 7) MoveSuitMan();
+        if (PhenomenonNumber == 8) AnimateMannequin();
+        if (PhenomenonNumber == 9) 
+        if (PhenomenonNumber == 10)
+        if (PhenomenonNumber == 11)
+        if (PhenomenonNumber == 12)
+        if (PhenomenonNumber == 13)
+    }
+
+    public void AnimateMannequin(bool isNormal)
+    {
+        ActiveteMannequin AM = Manenequin.GetComponent<ActivateMannequin>();
+        if (AM != null)
+        {
+            if (!isNormal) AM.enabled = true;
+            else AM.enabled = false;
+        }
+    }
+
+    public void MoveSuitMan(bool isNormal)
+    {
+
+    }
+
+    public void SpawnAndMoveZombie(bool isNormal)
+    {
+
+    }
+
+    public void TurnOffLight(bool isNormal)
+    {
+
+    }
+
+    public void Emergency(bool isNormal)
+    {
+
+    }
+
+    public void ComeUpFloor(bool isNormal)
+    {
+
+    }
+
+    public void PlayGhostSound(bool isNormal)
+    {
+
     }
 }

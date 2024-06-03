a96e57eda2d14585e0ad06d993ff194eb4b7bf98
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2024-06-03 14:50:35 +0900

    Feat | 바닥 상승 이상현상 구현 완료

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public GameObject Floor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        // 움직이는 발판 위에 있는 경우
+
+    }
+
+    void OnCollisionExit(Collision coll)
+    {
+        // 움직이는 발판에서 나갈 경우
+
+    }
+}
